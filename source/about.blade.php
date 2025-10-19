225f9a7321a383705b4d5c220b67230ce7a17a9e
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-10-18 20:00:57 -0400

    Add project listing component and update about page with project details

--- source/about.blade.php
+++ source/about.blade.php
@@ -10,6 +10,62 @@ description: Learn more about Daniel Haven, a software developer specializing in
       ['title' => 'Home', 'url' => '/', 'active' => false],
       ['title' => 'About', 'url' => '/about', 'active' => true],
   ];
+
+  $projects = [
+      [
+          'name' => 'Local X.com (Formerly Twitter) Mutelist',
+          'description' => 'A Chrome extension for hiding users on x.com; written in React/Typescript.',
+          'links' => [
+              ['title' => 'Chrome Web Store', 'url' => 'https://chromewebstore.google.com/detail/local-xcom-twitter-muteli/epgpnmkhgjnhhammgaaencaconefiokp'],
+          ],
+          'type' => 'published',
+      ],
+      [
+          'name' => 'Save To Play',
+          'description' => 'A browser extension to save YouTube videos to Play; written in Vue/Typescript.',
+          'links' => [
+              ['title' => 'GitHub Repository', 'url' => 'https://github.com/danielh-official/save-to-play'],
+              ['title' => 'Chrome Web Store', 'url' => 'https://chromewebstore.google.com/detail/unofficial-save-to-play/gdkaciebpaiikdhopaddmnpeoiedmfdj'],
+          ],
+          'type' => 'published',
+      ],
+      [
+          'name' => 'ynab-sdk-laravel',
+          'description' => 'An SDK for using the YNAB API in Laravel applications; written in PHP.',
+          'links' => [
+              ['title' => 'GitHub Repository', 'url' => 'https://github.com/ynab-sdk-laravel/ynab-sdk-laravel'],
+          ],
+          'type' => 'published',
+      ],
+      [
+          'name' => 'Streaks (For YNAB)',
+          'description' => "An iOS app that integrates with YNAB's API to show the user his or her spending habits; written in Swift.",
+          'links' => [
+              ['title' => 'Website', 'url' => 'https://streaksforynab.app/'],
+          ],
+          'type' => 'ongoing',
+      ],
+      [
+          'name' => 'Simple Funding Site',
+          'description' => 'Written in Laravel with React as the frontend and Inertia as the glue that holds backend and frontend together.',
+          'links' => [
+              ['title' => 'GitHub Repository', 'url' => 'https://github.com/danielh-official/simple-funding-site'],
+          ],
+          'type' => 'portfolio',
+      ],
+      [
+          'name' => 'Ruby on Rails Playground',
+          'description' => 'Dabbling in Ruby on Rails.',
+          'links' => [
+              ['title' => 'GitHub Repository', 'url' => 'https://github.com/danielh-official/ruby-on-rails-playground'],
+          ],
+          'type' => 'portfolio',
+      ],
+  ];
+
+  $publishedProjects = array_filter($projects, fn ($project) => $project['type'] === 'published');
+  $ongoingProjects = array_filter($projects, fn ($project) => $project['type'] === 'ongoing');
+  $portfolioProjects = array_filter($projects, fn ($project) => $project['type'] === 'portfolio');
 @endphp
 
 <x-breadcrumbs :items="$breadcrumbs" />
@@ -37,35 +93,9 @@ description: Learn more about Daniel Haven, a software developer specializing in
   <li><a href="https://pinkary.com/@danielhaven" target="_blank">Pinkary</a></li>
 </ul>
 
-<p>Published Projects:</p>
+<x-project-listing :projects="$publishedProjects" title="Published Projects" />
 
-<ul class="ml-16">
-  <li>
-    Local X.com (Twitter) Mutelist - Chrome extension for hiding users on x.com; written in React/Typescript (<a href="https://chromewebstore.google.com/detail/local-xcom-twitter-muteli/epgpnmkhgjnhhammgaaencaconefiokp" target="_blank">Chrome Web Store Link</a>)
-  </li>
-  <li>Save To Play - A browser extension to save YouTube videos to <a href="https://apps.apple.com/us/app/play-save-videos-watch-later/id1596506190" target="_blank">Play</a>; written in Vue/Typescript (<a href="https://github.com/danielh-official/save-to-play" target="_blank">GitHub Repository</a>, <a href="https://chromewebstore.google.com/detail/unofficial-save-to-play/gdkaciebpaiikdhopaddmnpeoiedmfdj" target="_blank">Chrome Web Store</a>)
-  </li>
-  <li>
-    ynab-sdk-laravel - An SDK for using the YNAB API in Laravel applications; written in PHP (<a href="https://github.com/ynab-sdk-laravel/ynab-sdk-laravel" target="_blank">GitHub Repository</a>)
-  </li>
-</ul>
+<x-project-listing :projects="$ongoingProjects" title="Ongoing Projects" />
 
-<p>Ongoing Projects:</p>
-
-<ul class="ml-16">
-  <li>
-    Streaks (For YNAB) - An iOS app that integrates with YNAB's API to show the user his or her spending habits; written in Swift (<a href="https://streaksforynab.app/" target="_blank">Website</a>)
-  </li>
-</ul>
-
-<p>Portfolio Projects:</p>
-
-<ul class="ml-16">
-  <li>
-    Simple Funding Site - Written in Laravel with React as the frontend and Inertia as the glue that holds backend and frontend together (<a href="https://github.com/danielh-official/simple-funding-site" target="_blank">GitHub Repository</a>)
-  </li>
-  <li>
-    Ruby on Rails Playground - Dabbling in Ruby on Rails (<a href="https://github.com/danielh-official/ruby-on-rails-playground" target="_blank">GitHub Repository</a>)
-  </li>
-</ul>
+<x-project-listing :projects="$portfolioProjects" title="Portfolio Projects" />
 @stop
