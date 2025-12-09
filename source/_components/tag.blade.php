331c8e9c1027a64a75b5b6125bb89208e8f4749b
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-09 14:49:18 -0500

    Add tag component and enhance work history records with tags and improved formatting

---
+++ source/_components/tag.blade.php
@@ -0,0 +1,4 @@
+<div class="inline-block px-2 py-1 mt-2 text-sm font-semibold text-white rounded-full"
+    style="background-color:{{ $bgColor ?? 'blue' }}">
+    {{ $slot }}
+</div>
--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -1,9 +1,9 @@
-<nav id="js-nav-menu" class="hidden w-auto px-2 pt-6 pb-2 bg-gray-200 shadow lg:hidden">
+<nav id="js-nav-menu" class="hidden w-auto px-2 pt-6 pb-2 bg-gray-200 shadow dark:bg-gray-700 dark:text-gray-200 lg:hidden">
     <ul class="my-0">
         @foreach ($page->getMenuItems() as $menuItem)
             <li class="pl-4">
                 <a title="{{ $page->siteName }} {{ $menuItem['name'] }}" href="{{ $menuItem['path'] }}"
-                    class="block mt-0 mb-4 text-sm no-underline {{ $page->isActive($menuItem['path']) ? 'active text-blue-500' : 'text-gray-800 hover:text-blue-500' }}">
+                    class="block mt-0 mb-4 text-sm no-underline {{ $page->isActive($menuItem['path']) ? 'active text-blue-500' : 'text-gray-800 dark:text-gray-200 hover:text-blue-500' }}">
                     {{ $menuItem['name'] }}
                 </a>
             </li>
