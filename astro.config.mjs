0aded66e8503a272605008f61379136c70d5eef3
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2026-02-20 08:44:48 -0500

    Update Astro configuration to set site URL for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    site: 'https://danielh-official.github.io',
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
