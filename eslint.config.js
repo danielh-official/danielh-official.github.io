a2ca36185a130b2cc8c72a6fcd32e663217bd8e8
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2026-04-02 05:33:57 -0400

    Fix linter error

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,15 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import tsParser from "@typescript-eslint/parser";
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+    },
+  },
   {
     rules: {
       // override/add rules settings here, such as:
